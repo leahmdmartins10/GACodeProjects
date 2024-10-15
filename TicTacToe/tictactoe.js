a5b58941fa7f13e2f38f8fc0a57188ab859464e0
leahmdmartins <leahmdmartins@icloud.com>
2024-10-15 18:33:47 -0400

    finished tic tac toe

--- TicTacToe/tictactoe.js
+++ TicTacToe/tictactoe.js
@@ -55,44 +55,89 @@ class TicTacToe {
     this.player1 = player1;
     this.player2 = player2;
     this.currentPlayer = player1; // we'll start with player 1, they're automatically X
+  }
+  placePiece(row, col) {
+    const player = this.currentPlayer;
+    if (player.placePiece(row, col, this.board)) {
+      this.updateCounters(row, col, player.theirCharacter);
+      if (this.checkWin(row, col, player.char)) {
+        console.log(`${player.playerName} wins!`);
+        this.displayBoard();
+        rl.close();
+        return true;
+      }
 
-    placePiece(row, col){
-      const player = this.currentPlayer;
-      if(player.placePiece(row, col, this.board)){
-        this.updateCounters(row, col, player.theirCharacter);
+      if (this.currentPlayer === player1) {
+        this.currentPlayer = player2;
+      } else {
+        this.currentPlayer = player1;
       }
     }
+    return false;
+  }
 
-    updateCounters(row, col, char){
-      this.rowCounts[char][row]++;
-      this.colCounts[char][col]++;
-      if(row === col){
-        this.LRDiag[char]++;
-      }
-      if(row + col == this.size - 1){
-        this.RLDiag[char]++;
-      }
+  updateCounters(row, col, char) {
+    this.rowCounts[char][row]++;
+    this.colCounts[char][col]++;
+    if (row === col) {
+      this.LRDiag[char]++;
     }
+    if (row + col == this.size - 1) {
+      this.RLDiag[char]++;
+    }
+  }
 
-    checkWin(row, col, char){
-      return (
-        this.rowCounts[char][row] === this.size ||
-        this.colCounts[char][col] == this.size ||
-        this.LRDiag[char] === this.size ||
-        this.RLDiag[char] === this.size
-      )
+  checkWin(row, col, char) {
+    return (
+      this.rowCounts[char][row] === this.size ||
+      this.colCounts[char][col] == this.size ||
+      this.LRDiag[char] === this.size ||
+      this.RLDiag[char] === this.size
+    );
+  }
+
+  displayBoard() {
+    console.log("Current board:");
+    for (let row = 0; row < this.size; row++) {
+      console.log(this.board[row].join(" "));
     }
   }
 }
 
-rl.question("What is player one's name: ", function(playerOneName){
-  rl.question("What is player two's name: ", function(playerTwoName){
-    let player1 = new Player(playerOneName, 'X', true);
-    let player2 = new Player(playerTwoName, 'O', false);
+rl.question("What is player one's name: ", function (playerOneName) {
+  rl.question("What is player two's name: ", function (playerTwoName) {
+    let player1 = new Player(playerOneName, "X", true);
+    let player2 = new Player(playerTwoName, "O", false);
 
-    console.log("Player 1's name is: ", player1.playerName, " and their piece is: ", player1.theirCharacter);
-    console.log("Player 2's name is: ", player2.playerName, " and their piece is: ", player2.theirCharacter);
+    // this is printing all the data we just got from the user
+    console.log(
+      "Player 1's name is: ",
+      player1.playerName,
+      " and their piece is: ",
+      player1.theirCharacter
+    );
+    console.log(
+      "Player 2's name is: ",
+      player2.playerName,
+      " and their piece is: ",
+      player2.theirCharacter
+    );
 
-    
+    const game = new TicTacToe(player1, player2);
+    game.displayBoard();
+
+    function gameLoop() {
+      rl.question(
+        `${game.currentPlayer.playerName} please enter a row and column (e.g., 1 2) that you would like to place a piece in.`,
+        function (input) {
+          const [row, col] = input.split(" ").map(Number);
+          if (game.placePiece(row, col)) {
+            return;
+          }
+          game.displayBoard();
+          gameLoop();
+        }
+      );
+    }
   });
 });
