8f6b08827f3b229a1f1a72cb11207be37d0e091b
leahmdmartins <leahmdmartins@icloud.com>
2024-11-07 20:52:07 -0500

    added a drop down option for the dog breeds and commented the code using codium:

--- dog-collector/js/main.js
+++ dog-collector/js/main.js
@@ -1,29 +1,39 @@
-
-// defines all of the variables on the site
+// Select the necessary elements from the DOM
 const button = document.querySelector('button');
-const breedInput = document.querySelector('input');
+const breedSelect = document.querySelector('select');
 const imageDiv = document.querySelector('div');
 
+// Load the breed list when the window loads
 window.onload = getBreeds;
 
-// adds an event listener for the button click
+// Add a click event listener to the button
 button.addEventListener('click', async function() {
-    let breed = breedInput.value.toLowerCase();
+    // Get the selected breed from the dropdown
+    let breed = breedSelect.value;
+    // Construct the URL for fetching a random dog image of the selected breed
     const url = `https://dog.ceo/api/breed/${breed}/images/random`;
     console.log(url);
 
+    // Fetch the random dog image
     let response = await fetch(url);
     const dogPic = await response.json();
 
+    // Display the fetched image in the imageDiv
     imageDiv.innerHTML = `<img src="${dogPic.message}">`;
-
 })
 
+// Function to fetch and populate the breed list
 async function getBreeds(){
+    // Fetch the list of all breeds
     const response = await fetch('https://dog.ceo/api/breeds/list/all');
     const data = await response.json();
-
     const breeds = Object.keys(data.message);
-    console.log(breeds);
 
+    // Populate the dropdown with breed options
+    breeds.forEach((breed) => {
+        const option = document.createElement('option');
+        option.value = breed;
+        option.innerText = breed;
+        breedSelect.appendChild(option);
+    })
 }
